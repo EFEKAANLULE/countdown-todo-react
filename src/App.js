1a24ae1ee14b3026416c314b3f9da634745b30aa
EFEKAANLULE <101347167+EFEKAANLULE@users.noreply.github.com>
2023-02-28 11:58:59 +0300

    Deneme feat timer git push i

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   const [tasks, setTasks] = useState([]); // empty array to give the tasks to  // By default the tasks array is empty 
-  const [intervals, setIntervals] = useState([]);  // time control
+  const [intervals, setIntervals] = useState([]);  
 
   const addTask = (title, countdown) => {
     const newTask = {
@@ -15,10 +15,36 @@ function App() {
       done: false,
       timeLeft: countdown,
     };
+
+    // add the new task to the state
     setTasks((prevTasks) => [...prevTasks, newTask]);
 
-  }
-  
+    // update the countdown for the task every second
+    const intervalId = setInterval(() => { // id for the time, sort of 
+      setTasks((prevTasks) => {
+        return prevTasks.map((task) => { //for every task in the prevtask array we check whether the countdown is reached 0, if it is it is 'DONE'
+          if (task.id === newTask.id) {
+            if (task.timeLeft === 0) {
+              task.done = true;
+            } else {
+              task.timeLeft -= 1;
+            }
+          }
+          return task;
+        });
+      });
+    }, 1000);
+
+     // add the interval ID to the state
+    setIntervals((prevIntervals) => [...prevIntervals, intervalId]);
+  };
+  //add task function is finished.
+
+  useEffect(() => { // by using useEffect, we can ensure that countdown timers are updated every second, even if the user adds or removes tasks from the array.
+    return () => {
+      intervals.forEach((intervalId) => clearInterval(intervalId)); // memory efficient ? 
+    };
+  }, [intervals]);
 
   return (
     <div className="App">
